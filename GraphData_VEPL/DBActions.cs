a3a0fddd5dc2a2047ce0bc2616e6f4100d91ca8b
itrustinfo <33476551+itrustinfo@users.noreply.github.com>
2023-10-18 18:49:10 +0530

    18th oct latest
    subbmittala add for VEPL

---
+++ GraphData_VEPL/DBActions.cs
@@ -0,0 +1,527 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Threading;
+
+namespace GraphData
+{
+    public class DBActions
+    {
+        public string GetConnectionString()
+        {
+           
+            return System.Configuration.ConfigurationManager.ConnectionStrings["PMConnectionString"].ToString();
+
+        }
+
+
+        public DataSet GetWorkPackages_By_ProjectUID(Guid ProjectUID)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("Usp_getWorkPackge_by_ProjectUID", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.SelectCommand.Parameters.AddWithValue("@ProjectUID", ProjectUID);
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+                Console.WriteLine("Error : " + ex.Message);
+            }
+            return ds;
+        }
+
+        public string getWorkPackageNameby_WorkPackageUID(Guid WorkPackageUID)
+        {
+            string sUser = "";
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                if (con.State == ConnectionState.Closed) con.Open();
+                SqlCommand cmd = new SqlCommand("usp_getWorkPackageNameby_WorkPackageUID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@WorkPackageUID", WorkPackageUID);
+                sUser = (string)cmd.ExecuteScalar();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                sUser = "Error : " + ex.Message;
+            }
+            return sUser;
+        }
+
+        public DataSet GetAllDelayedTasks_by_WorkPackageUID(Guid WorkPackageUID)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetAllDelayedTasks_by_WorkPackageUID", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.SelectCommand.Parameters.AddWithValue("@WorkPackageUID", WorkPackageUID);
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+
+                Console.WriteLine("Error : in delay " + ex.Message);
+                Thread.Sleep(10000);
+            }
+            return ds;
+        }
+
+        public string getTaskParents(Guid TaskUID)
+        {
+            string ParentTasks = "";
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                if (con.State == ConnectionState.Closed) con.Open();
+                SqlCommand cmd = new SqlCommand("usp_getTaskParentNames", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@TaskUID", TaskUID);
+                ParentTasks = (string)cmd.ExecuteScalar();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                ParentTasks = "Error : " + ex.Message;
+            }
+            return ParentTasks;
+        }
+
+        public string getUserEmails(Guid TaskUID)
+        {
+            string ParentTasks = "";
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                if (con.State == ConnectionState.Closed) con.Open();
+                SqlCommand cmd = new SqlCommand("usp_getUserEmails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@TaskUID", TaskUID);
+                ParentTasks = (string)cmd.ExecuteScalar();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                ParentTasks = "";
+            }
+            return ParentTasks;
+        }
+
+        public string SendMail(string toEmailID, string Subject, string sHtmlString, string cc, string Attachment)
+        {
+            try
+            {
+                MailMessage mm = new MailMessage();
+                DataTable dtemailCred = GetEmailCredentials();
+                mm.To.Add(toEmailID);
+                mm.From = new MailAddress(dtemailCred.Rows[0][0].ToString(), "Project Manager");
+                mm.Subject = Subject;
+                if (cc != "")
+                {
+                    string[] CCId = cc.Split(',');
+                    foreach (string CCEmail in CCId)
+                    {
+                        mm.CC.Add(new MailAddress(CCEmail)); //Adding Multiple CC email Id
+                    }
+                }
+                if (Attachment != "")
+                {
+                    mm.Attachments.Add(new Attachment(Attachment));
+                }
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+                client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                client.EnableSsl = true;
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(dtemailCred.Rows[0][0].ToString(), dtemailCred.Rows[0][1].ToString());
+                client.UseDefaultCredentials = false;
+                client.Credentials = credentials;
+                mm.IsBodyHtml = true;
+                mm.Body = string.Format(sHtmlString);
+                client.Send(mm);
+                return "Success";
+            }
+            catch (Exception ex)
+            {
+                return "Failure : " + ex.Message;
+            }
+        }
+
+        public DataTable GetEmailCredentials()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("Usp_GetMailCredentials", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            return dt;
+        }
+
+        public string getUserEmail_by_UserUID(Guid UserUID)
+        {
+            string ParentTasks = "";
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                if (con.State == ConnectionState.Closed) con.Open();
+                SqlCommand cmd = new SqlCommand("usp_getUserEmail_By_UserUID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserUID", UserUID);
+                ParentTasks = (string)cmd.ExecuteScalar();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                ParentTasks = "";
+            }
+            return ParentTasks;
+        }
+
+        public int checkAlertMailSent(Guid TaskUID)
+        {
+            int Cnt = 0;
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                if (con.State == ConnectionState.Closed) con.Open();
+                SqlCommand cmd = new SqlCommand("usp_checkAlertMailSent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@TaskUID", TaskUID);
+                Cnt = (int)cmd.ExecuteScalar();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Cnt = 0;
+            }
+            return Cnt;
+        }
+
+        // added on 18/11/2020
+        public DataTable getAllTasksForAlert()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetAllTasksForAlert", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            return dt;
+
+        }
+
+        public DataTable getAllBankGuarantees()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetAllBankGuarantees", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            return dt;
+
+        }
+
+        public DataTable getAllInsurances()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetAllInsurances", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            return dt;
+
+        }
+
+        public DataTable getAllIssues()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetAllIssues", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            return dt;
+
+        }
+
+        public DataSet GetTaskScheduleDatesforGraph(Guid WorkpackageUID)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetTaskScheduleDatesforGraph", con);
+                cmd.SelectCommand.Parameters.AddWithValue("@WorkpackageUID", WorkpackageUID);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+            }
+            return ds;
+        }
+
+        public DataSet GetTaskScheduleValuesForGraph(Guid WorkpackageUID, DateTime startdate, DateTime enddate)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetTaskScheduleValuesForGraph", con);
+                cmd.SelectCommand.Parameters.AddWithValue("@WorkPackageUID", WorkpackageUID);
+                cmd.SelectCommand.Parameters.AddWithValue("@StartDate", startdate);
+                cmd.SelectCommand.Parameters.AddWithValue("@EndDate", enddate);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+            }
+            return ds;
+        }
+
+        public DataSet GetWorkPackages_By_WorkPackageUID(Guid WorkPackageUID)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("Usp_getWorkPackge_by_WorkPackageUID", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.SelectCommand.Parameters.AddWithValue("@WorkPackageUID", WorkPackageUID);
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+            }
+            return ds;
+        }
+
+        public DataSet GetTaskActualValuesForGraph(Guid WorkpackageUID, DateTime startdate, DateTime enddate)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetTaskActualValuesForGraph", con);
+                cmd.SelectCommand.Parameters.AddWithValue("@WorkPackageUID", WorkpackageUID);
+                cmd.SelectCommand.Parameters.AddWithValue("@StartDate", startdate);
+                cmd.SelectCommand.Parameters.AddWithValue("@EndDate", enddate);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+            }
+            return ds;
+        }
+
+        public DataSet GetTaskRevisedScheduleValuesForGraph(Guid WorkpackageUID, DateTime startdate, DateTime enddate)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+               // SqlDataAdapter cmd = new SqlDataAdapter("GetTaskRevisedScheduleValuesForGraph", con);
+                SqlDataAdapter cmd = new SqlDataAdapter("usp_GetTaskRevisedScheduleValuesForGraph", con);
+                cmd.SelectCommand.Parameters.AddWithValue("@WorkPackageUID", WorkpackageUID);
+                cmd.SelectCommand.Parameters.AddWithValue("@StartDate", startdate);
+                cmd.SelectCommand.Parameters.AddWithValue("@EndDate", enddate);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+            }
+            return ds;
+        }
+
+        //public int InsertGraphPhysicalProgressValues(Guid PhysicalProgress_UID, Guid ProjectUID, Guid WorkPackageUID, DateTime Start_Date, decimal PlanValue,decimal CumPlanValue,decimal AchValue,decimal CumAchValue)
+        //{
+        //    int cnt = 0;
+        //    try
+        //    {
+        //        using (SqlConnection con = new SqlConnection(GetConnectionString()))
+        //        {
+
+        //            using (SqlCommand cmd = new SqlCommand("InsertPhysicalProgressGraphValues"))
+        //            {
+        //                cmd.CommandType = CommandType.StoredProcedure;
+        //                cmd.Connection = con;
+        //                cmd.Parameters.AddWithValue("@GraphPhysicalProgressUID", PhysicalProgress_UID);
+        //                cmd.Parameters.AddWithValue("@ProjectUID", ProjectUID);
+        //                cmd.Parameters.AddWithValue("@WorkPackageUID", WorkPackageUID);
+        //                cmd.Parameters.AddWithValue("@StartDate", Start_Date);
+        //                cmd.Parameters.AddWithValue("@PlanValue", PlanValue);
+        //                cmd.Parameters.AddWithValue("@CumPlanValue", CumPlanValue);
+        //                cmd.Parameters.AddWithValue("@AchValue", AchValue);
+        //                cmd.Parameters.AddWithValue("@CumAchValue", CumAchValue);
+
+        //                con.Open();
+        //                cnt = cmd.ExecuteNonQuery();
+        //                con.Close();
+        //            }
+        //        }
+        //        return cnt;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return cnt;
+        //    }
+        //}
+
+        public int InsertGraphPhysicalProgressValues(Guid PhysicalProgress_UID, Guid ProjectUID, Guid WorkPackageUID, DateTime Created_Date, string ValuesString)
+        {
+            int cnt = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(GetConnectionString()))
+                {
+
+                    using (SqlCommand cmd = new SqlCommand("InsertPhysicalProgressGraphValues"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        cmd.Parameters.AddWithValue("@GraphPhysicalProgressUID", PhysicalProgress_UID);
+                        cmd.Parameters.AddWithValue("@ProjectUID", ProjectUID);
+                        cmd.Parameters.AddWithValue("@WorkPackageUID", WorkPackageUID);
+                        cmd.Parameters.AddWithValue("@CreatedDate", Created_Date);
+                        cmd.Parameters.AddWithValue("@ValuesString", ValuesString);
+                       
+                        con.Open();
+                        cnt = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                return cnt;
+            }
+            catch (Exception ex)
+            {
+                return cnt;
+            }
+        }
+
+
+        public int DeleteGraphPhysicalProgressValues(Guid ProjectUID, Guid WorkPackageUID)
+        {
+            int cnt = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(GetConnectionString()))
+                {
+
+                    using (SqlCommand cmd = new SqlCommand("DeletePhysicalProgressGraphValues"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        cmd.Parameters.AddWithValue("@ProjectUID", ProjectUID);
+                        cmd.Parameters.AddWithValue("@WorkPackageUID", WorkPackageUID);
+                        
+                        con.Open();
+                        cnt = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                return cnt;
+            }
+            catch (Exception ex)
+            {
+                return cnt;
+            }
+        }
+
+        public int DeleteGraphPhysicalProgressValues()
+        {
+            int cnt = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(GetConnectionString()))
+                {
+
+                    using (SqlCommand cmd = new SqlCommand("DeletePhysicalProgressGraphValues"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        
+                        con.Open();
+                        cnt = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                return cnt;
+            }
+            catch (Exception ex)
+            {
+                return cnt;
+            }
+        }
+
+        public DataSet GetWorkPackages()
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlDataAdapter cmd = new SqlDataAdapter("GetWorkPackges", con);
+                cmd.SelectCommand.CommandType = CommandType.StoredProcedure;
+                cmd.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds = null;
+                Console.WriteLine("Error : " + ex.Message);
+            }
+
+            return ds;
+        }
+
+
+    }
+}
---
+++ VVSTP_Submittals_Add/VVSTP_Submittals_Add/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVSTP_Submittals_Add
+{
+    class Program
+    {
+        static Guid workPackageId = new Guid("5124CA05-57F1-4BF2-A4AA-FCEE96642AAF");
+        static Guid projectId = new Guid("E12C47F9-F3B5-45D5-98CB-B438F48F32A8");
+        static void Main(string[] args)
+        {
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "\\MDL 150 MLD STP V Valley-R1_new.xlsx";
+            Program pg = new Program();
+            pg.ReadExcel(fileName, ".xlsx");
+        }
+
+
+        private void ReadExcel(string fileName, string fileExt)
+        {
+            string conn = string.Empty;
+            DataTable dtexcel = new DataTable();
+            if (fileExt.CompareTo(".xls") == 0)
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+            else
+                conn = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0 Xml;HDR=YES'");
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [MDL 150 MLD STP V Valley-R1$]", con); //here we read data from sheet1  
+                    oleAdpt.Fill(dtexcel);
+
+                    string refno = string.Empty;
+                    string SubmittalName = string.Empty;
+                    string TaskUID = string.Empty;
+                    string CategoryUID = string.Empty;
+                    string TargetDate = string.Empty;
+                    foreach(DataRow dr in dtexcel.Rows)
+                    {
+                        if(dr[0].ToString() == "Y")
+                        {
+                            refno = dr[2].ToString()  + dr[3].ToString() +  dr[4].ToString()  + dr[5].ToString()  + "/" + dr[6].ToString() + "/"  + dr[7].ToString();
+                            SubmittalName = dr[8].ToString();
+                            TaskUID = dr[11].ToString();
+                            CategoryUID = dr[12].ToString();
+                            TargetDate = dr[13].ToString();
+                            //
+                            Guid SubmittalUID = Guid.NewGuid();
+                            addsubmittal(SubmittalUID, refno,SubmittalName,new Guid(TaskUID), new Guid(CategoryUID),TargetDate);
+                            //
+                            addsubmittalUsers(SubmittalUID);
+                        }
+                        else
+                        {
+                            string str = dr[0].ToString();
+                            string test = "";
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+             }
+        }
+
+        public int addsubmittal(Guid DocumentUID,string DocRefNumber,string SubmittalName,Guid TaskUID,Guid CategoryUID,string TargetDate)
+        {
+            int cnt = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(GetConnectionString()))
+                {
+
+                    using (SqlCommand cmd = new SqlCommand("usp_VVSTP_Submittaladd"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        cmd.Parameters.AddWithValue("@DocumentUID", DocumentUID);
+                        cmd.Parameters.AddWithValue("@DocRefNumber", DocRefNumber);
+                        cmd.Parameters.AddWithValue("@SubmittalName", SubmittalName);
+                        cmd.Parameters.AddWithValue("@TaskUID", TaskUID);
+                        cmd.Parameters.AddWithValue("@CategoryUID", CategoryUID);
+                        if(!string.IsNullOrEmpty(TargetDate))
+                        {
+                            cmd.Parameters.AddWithValue("@Step1TargetDate", TargetDate);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@Step1TargetDate", DateTime.Now.AddDays(1));
+                        }
+                        con.Open();
+                        cnt = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                return cnt;
+            }
+            catch (Exception ex)
+            {
+                return cnt;
+            }
+        }
+
+        public int addsubmittalUsers(Guid SubmittalUID)
+        {
+            int cnt = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(GetConnectionString()))
+                {
+
+                    using (SqlCommand cmd = new SqlCommand("usp_addVSTPsubmittal_Users"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                       
+                        cmd.Parameters.AddWithValue("@SubmittalUID", SubmittalUID);
+                        con.Open();
+                        cnt = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                return cnt;
+            }
+            catch (Exception ex)
+            {
+                return cnt;
+            }
+        }
+
+        public string GetConnectionString()
+        {
+
+            return System.Configuration.ConfigurationManager.ConnectionStrings["PMConnectionString"].ToString();
+
+        }
+
+
+    }
+}
